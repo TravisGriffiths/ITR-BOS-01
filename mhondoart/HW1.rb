8ebba8cb05d8e8f5b196d692733920baa516a735
maryann hondo <mhondo@art.home>
2013-03-20 13:28:31 -0400

    my first assignment

--- mhondoart/HW1.rb
+++ mhondoart/HW1.rb
@@ -46,6 +46,15 @@
 puts "Hello and Welcome to the Secret Number Game!"
 first_name = "Maryann"
 last_name = "Hondo"
-puts "this game was created by" + first_name + " " + last_name
+puts "this game was created by" + first_name + " " + last_name + " "
+print "to play the game, pick a number between 1 & 20 and hit enter "
+iguess = gets.to_i
+rnum = [1...20]
+newrandom = rand (20)
+if ( rnum[newrandom] != iguess)
+puts "wrong number try again!"
+else
+puts "you got it!"
+end
 puts "the end"
 
