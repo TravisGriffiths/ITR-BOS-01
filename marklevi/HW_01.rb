3c565f8040d64fcd91b8857e6ac9631ac5d98c45
marklevi <mdayanlevi@gmail.com>
2013-03-20 18:20:42 -0400

    adding homework 1

---
+++ marklevi/HW_01.rb
@@ -0,0 +1,29 @@
+###############################################################################
+#
+# Introduction to Ruby on Rails
+#
+# HW 01
+#
+# Purpose:
+#
+# Read the taks below and complete the exercises in this file. We will start
+# to write the beginnings of our "Secret Number Game" using what we've
+# learned in Ruby Lesson 01.
+#
+###############################################################################
+
+
+	# My name is Mark Levi. I am a recent graduate of Brandeis University. I am interested in coding. 
+	puts "Welcome to my Secret Number Game"
+	first_name = "Mark"
+	last_name = "Levi"
+	puts "Created by " + first_name + " " + last_name
+
+
+
+###############################################################################
+#
+# Student Solution
+#
+###############################################################################
+
