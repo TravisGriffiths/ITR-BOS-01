5bdc56a6b3571ff89b2fd0b61d8e2e395009b703
Sanders Kleinfeld <sanders@oreilly.com>
2013-03-25 18:27:16 -0400

    Adding in baseline HW2 solutions.

--- sandersk/HW3.rb
+++ sandersk/HW3.rb
@@ -51,3 +51,38 @@
 #Put your solution below this line.
 #
 ###############################################################################
+
+puts "Welcome to the secret number game. I will be your host"
+
+puts "What is your name"
+
+player_name = gets.chomp
+
+puts "Hi there #{player_name}! You have 3 guesses to guess the secret number, which is between 1 and 10"
+
+guesses_left = 3
+SECRET_NUMBER = 9
+
+while guesses_left > 0 
+  puts "You have #{guesses_left} guesses left"
+  puts "Enter your guess"
+  player_guess_as_int = gets.to_i
+  unless (0 < player_guess_as_int) and (player_guess_as_int <= 10)
+    puts "Your guess is not a number between 1 and 10. Try again"
+    next
+  end
+  if player_guess_as_int == SECRET_NUMBER
+    puts "Correct! The number was #{SECRET_NUMBER}. You won!"
+    # Quit script entirely on a win
+    exit 0
+  elsif player_guess_as_int > SECRET_NUMBER
+    puts "Try lower next time"
+    guesses_left -= 1
+  else
+   puts "Try higher next time"
+   guesses_left -= 1
+  end
+end
+
+# If loop completed, we're out of guesses and player lost
+puts "Sorry, you lose. The secret number was #{SECRET_NUMBER}"
