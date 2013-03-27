ee3713a50c9a095c3cc41894fe4f572029ef5fdc
Sanders Kleinfeld <sanders@oreilly.com>
2013-03-27 18:31:32 -0400

    Copying over HW3 solution.

--- sandersk/HW4.rb
+++ sandersk/HW4.rb
@@ -34,3 +34,45 @@
 # Student's Solution
 #
 ###############################################################################
+
+secret_number = set_of_numbers.sample()
+
+messages = {:win => "Hooray! You correctly guessed the secret number",
+	    :lose => "Sorry, you ran out of guesses. The secret number was #{secret_number}.",
+	    :too_low => "Your guess was too low. Try a higher number.",
+	    :too_high => "Your guess was too high. Try a lower number."
+	    }
+ 
+guesses_left = 3
+
+puts "Welcome to the secret number game. I will be your host"
+
+puts "What is your name"
+
+player_name = gets.chomp
+
+puts "Hi there #{player_name}! You have 3 guesses to guess the secret number, which is between 1 and 10"
+
+while guesses_left > 0 
+  puts "You have #{guesses_left} guesses left"
+  puts "Enter your guess"
+  player_guess_as_int = gets.to_i
+  unless (0 < player_guess_as_int) and (player_guess_as_int <= 10)
+    puts "Your guess is not a number between 1 and 10. Try again"
+    next
+  end
+  if player_guess_as_int == secret_number
+   puts messages[:win]
+    # Quit script entirely on a win
+    exit 0
+  elsif player_guess_as_int > secret_number
+    puts messages[:too_high]
+    guesses_left -= 1
+  else
+   puts messages[:too_low]
+   guesses_left -= 1
+  end
+end
+
+# If loop completed, we're out of guesses and player lost
+puts messages[:lose]
