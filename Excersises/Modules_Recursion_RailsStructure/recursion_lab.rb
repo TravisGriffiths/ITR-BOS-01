0c47649a52ca91877c3144482c5b1629c401b3c9
Travis Griffiths <travis@quant5.com>
2013-04-08 20:18:17 -0400

    recursion lab

--- Excersises/Modules_Recursion_RailsStructure/recursion_lab.rb
+++ Excersises/Modules_Recursion_RailsStructure/recursion_lab.rb
@@ -7,7 +7,20 @@
 	################################################################################
 
 	def is_palindrome?(candidate_string)
-		candidate_char_array = candidate_string.split('') unless candidate_string.is_a? Array
+		unless candidate_string.is_a? Array
+			candidate_char_array = candidate_string.split('') 
+		else
+			candidate_char_array = candidate_string
+		end
 		#Fill in the rest such that this method returns true for palindromes and false otherwise
-
+		# CASE 1 size 0 -> true 
+		return true if candidate_char_array.length == 0
+		# CASE 2 size 1 -> true
+		return true if candidate_char_array.length == 1
+		# CASE 3 a.slice(1, (a.length - 2))
+		if(candidate_char_array[0] == candidate_char_array[-1])
+			return is_palindrome? candidate_char_array.slice(1, (candidate_char_array.length - 2))
+		else
+			return false
+		end
 	end
