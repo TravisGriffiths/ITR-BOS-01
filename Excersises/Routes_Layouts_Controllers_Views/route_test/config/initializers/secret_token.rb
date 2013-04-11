160b1a7bce593127d07d150db6e769cfcd8fbcff
Travis Griffiths <travis@quant5.com>
2013-04-10 20:45:23 -0400

    Added new links and routs rails app

---
+++ Excersises/Routes_Layouts_Controllers_Views/route_test/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RouteTest::Application.config.secret_token = 'f5ccfbb2ee87858020c273d180e2a15480b99a7e0b6ed7097bb2ef90f2569d3675d748c2d855ae9b6c0b4e28db2b5aedfd4287062ef2eb2f3060a776610a6d7b'
