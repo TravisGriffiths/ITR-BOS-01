485531cdcea3bcf52edf0077e3604d1b2b63d9b1
Travis Griffiths <travis@quant5.com>
2013-05-03 09:20:03 -0400

    Added the Ajax test app from class

---
+++ Excersises/Single_Page_Applications/ajax_test/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AjaxTest::Application.config.secret_token = '4c4cda89241df78c55ed4be3c001b8b4f46da991714134d9a87162251d2043204c404ebdd58b7a0a4124dff696b4bc18dd536da15656eadae408294100d5e2fe'
